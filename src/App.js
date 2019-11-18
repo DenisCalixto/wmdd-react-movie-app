c448672fa8c5d34adcae9be28219dd17153c85c6
DenisCalixto <deniscalixto@gmail.com>
2019-11-17 17:49:39 -0800

    Tabs version 01

--- src/App.js
+++ src/App.js
@@ -1,15 +1,60 @@
 import React, { Component } from 'react'
 import './App.css';
+import PropTypes from 'prop-types';
+import { makeStyles } from '@material-ui/core/styles';
+import AppBar from '@material-ui/core/AppBar';
+import Tabs from '@material-ui/core/Tabs';
+import Tab from '@material-ui/core/Tab';
+import Typography from '@material-ui/core/Typography';
+import Box from '@material-ui/core/Box';
+
+import SearchResultContainer from './containers/SearchResultContainer';
 import MoviesContainer from './containers/MoviesContainer'
 import TVShowsContainer from './containers/TVShowsContainer'
 import SearchForm from './components/SearchForm';
-import SearchResultContainer from './containers/SearchResultContainer';
-
 import { searchContent } from './services/api'
+
+function TabPanel(props) {
+  const { children, value, index, ...other } = props;
+
+  return (
+    <Typography
+      component="div"
+      role="tabpanel"
+      hidden={value !== index}
+      id={`simple-tabpanel-${index}`}
+      aria-labelledby={`simple-tab-${index}`}
+      {...other}
+    >
+      <Box p={3}>{children}</Box>
+    </Typography>
+  );
+}
+
+TabPanel.propTypes = {
+  children: PropTypes.node,
+  index: PropTypes.any.isRequired,
+  value: PropTypes.any.isRequired,
+};
+
+function a11yProps(index) {
+  return {
+    id: `simple-tab-${index}`,
+    'aria-controls': `simple-tabpanel-${index}`,
+  };
+}
+
+const useStyles = makeStyles(theme => ({
+  root: {
+    flexGrow: 1,
+    backgroundColor: theme.palette.background.paper,
+  },
+}));
 
 class App extends Component {
   state = {
     search_results: [],
+    value: 0,
   }
   category = ""
   searchText = ""
@@ -22,6 +67,10 @@ class App extends Component {
      this.searchResultRef = React.createRef()
   }
 
+  handleChange = (event, value) => {
+    this.setState( (state) => ({value}));
+  };
+
   handleSearch = () => {
     // console.log(this.category)
     // console.log(this.searchText)
@@ -31,6 +80,9 @@ class App extends Component {
       })
       // console.log(this.state.search_results)
       this.searchResultRef.current.updateSearchResults(this.state.search_results)
+      this.setState({
+        value: 1
+      })
     })
   }
 
@@ -58,13 +110,31 @@ class App extends Component {
           onInputChange={this.handleSearchText}
           onChangeCategory={this.handleCategoryChange}
           onSearch={this.handleSearch}
+          category={"movie"}
         />
-        <SearchResultContainer 
-          ref={this.searchResultRef} 
-          search_results={this.state.search_results} 
-        />
-        <MoviesContainer category={"now_playing"} />
-        <TVShowsContainer category={"top_rated"} />
+        <AppBar position="static">
+          <Tabs value={this.state.value} 
+            onChange={this.handleChange} 
+            variant="fullWidth"
+            centered
+            aria-label="simple tabs example">
+            <Tab label="MOVIES" {...a11yProps(0)} />
+            <Tab label="SEARCH RESULTS" {...a11yProps(1)} />
+            <Tab label="TV SHOWS" {...a11yProps(2)} />
+          </Tabs>
+        </AppBar>
+        <TabPanel value={this.state.value} index={0}>
+          <MoviesContainer category={"now_playing"} />
+        </TabPanel>
+        <TabPanel value={this.state.value} index={1}>
+          <SearchResultContainer 
+            ref={this.searchResultRef} 
+            search_results={this.state.search_results}
+          />
+        </TabPanel>
+        <TabPanel value={this.state.value} index={2}>
+          <TVShowsContainer category={"top_rated"} />
+        </TabPanel>
       </div>
     )
   }
